c97ac7b388a5331632a134649509bad3d0c5755f
karim2422 <karimelsa38@gmail.com>
2025-02-20 00:43:29 +0200

    hide key

--- lib/core/constants/endpoints_strings.dart
+++ lib/core/constants/endpoints_strings.dart
@@ -1,8 +1,8 @@
-//https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=lWgYbAm8gJ4Rj2FdIKiJ2TzXckEDHFSJ
+//https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=
 
 abstract class EndpointsStrings {
   static const String baseUrl = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/';
-  static const String apiKey = 'lWgYbAm8gJ4Rj2FdIKiJ2TzXckEDHFSJ';
+  static const String apiKey = '';
   static const String apiKeyParam ='api-key';
   static const String jsonFormat = '.json'; 
 
